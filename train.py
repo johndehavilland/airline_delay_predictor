f233ed7fd0516dde859935a5ad15169554e0c3f0
johndehavilland <johndehavilland@gmail.com>
2017-12-20 08:53:50 -0500

    initial commit

--- train.py
+++ train.py
@@ -21,3 +21,30 @@ print(args)
 
 # Create the outputs folder - save any outputs you want managed by AzureML here
 os.makedirs('./outputs', exist_ok=True)
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import itertools
+
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+from sklearn.metrics import confusion_matrix
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras import utils
+
+# Use the Azure Machine Learning data preparation package
+from azureml.dataprep import package
+
+# Use the Azure Machine Learning data collector to log various metrics
+from azureml.logging import get_azureml_logger
+logger = get_azureml_logger()
+
+# This call will load the referenced package and return a DataFrame.
+# If run in a PySpark environment, this call returns a
+# Spark DataFrame. If not, it will return a Pandas DataFrame.
+df = package.run('prep_airline_data.dprep', dataflow_idx=0)
+
+# Remove this line and add code that uses the DataFrame
+df.head(10)
